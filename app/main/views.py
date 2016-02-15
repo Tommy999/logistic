ac117e615c1af58451dc1c08737a45759b9f6751
Tommy999 <karpenko_tolia4@ukr.net>
2016-02-15 11:15:57 +0200

    skeleton for main page

--- app/main/views.py
+++ app/main/views.py
@@ -17,3 +17,23 @@ def index_raw():
 
     return render_template('index_raw.html')
 
+@main.route('/index_raw_2', methods=['GET', 'POST'])
+def index_raw_2():
+
+    return render_template('index_raw_2.html')
+
+@main.route('/index_raw_3', methods=['GET', 'POST'])
+def index_raw_3():
+
+    return render_template('index_raw_3.html')
+
+@main.route('/index_raw_4', methods=['GET', 'POST'])
+def index_raw_4():
+
+    return render_template('index_raw_4.html')
+
+@main.route('/index_raw_5', methods=['GET', 'POST'])
+def index_raw_5():
+
+    return render_template('index_raw_5.html')
+
